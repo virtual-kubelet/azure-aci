2b765789940ba1374af0bbe2791f9605beed5c18
Brian Goff <cpuguy83@gmail.com>
2019-06-12 05:11:49 -0700

    Move around some packages (#658)
    * Move tracing exporter registration
    
    This doesn't belong in the library and should be configured by the
    consumer of the opencensus package.
    
    * Rename `vkublet` package to `node`
    
    `vkubelet` does not convey any information to the consumers of the
    package.
    Really it would be nice to move this package to the root of the repo,
    but then you wind up with... interesting... import semantics due to the
    repo name... and after thinking about it some, a subpackage is really
    not so bad as long as it has a name that convey's some information.
    
    `node` was chosen since this package deals with all the semantics of
    operating a node in Kubernetes.

---
+++ virtual-kubelet/commands/root/tracing_register_ocagent.go
@@ -0,0 +1,36 @@
+// +build !no_ocagent_exporter
+
+package root
+
+import (
+	"os"
+
+	"contrib.go.opencensus.io/exporter/ocagent"
+	"github.com/virtual-kubelet/virtual-kubelet/errdefs"
+	"go.opencensus.io/trace"
+)
+
+func init() {
+	RegisterTracingExporter("ocagent", NewOCAgentExporter)
+}
+
+// NewOCAgentExporter creates a new opencensus tracing exporter using the opencensus agent forwarder.
+func NewOCAgentExporter(opts TracingExporterOptions) (trace.Exporter, error) {
+	agentOpts := append([]ocagent.ExporterOption{}, ocagent.WithServiceName(opts.ServiceName))
+
+	if endpoint := os.Getenv("OCAGENT_ENDPOINT"); endpoint != "" {
+		agentOpts = append(agentOpts, ocagent.WithAddress(endpoint))
+	} else {
+		return nil, errdefs.InvalidInput("must set endpoint address in OCAGENT_ENDPOINT")
+	}
+
+	switch os.Getenv("OCAGENT_INSECURE") {
+	case "0", "no", "n", "off", "":
+	case "1", "yes", "y", "on":
+		agentOpts = append(agentOpts, ocagent.WithInsecure())
+	default:
+		return nil, errdefs.InvalidInput("invalid value for OCAGENT_INSECURE")
+	}
+
+	return ocagent.NewExporter(agentOpts...)
+}
---
+++ virtual-kubelet/commands/root/tracing_register_test.go
@@ -0,0 +1,44 @@
+package root
+
+import (
+	"testing"
+
+	"github.com/virtual-kubelet/virtual-kubelet/errdefs"
+	"go.opencensus.io/trace"
+)
+
+func TestGetTracingExporter(t *testing.T) {
+	defer delete(tracingExporters, "mock")
+
+	mockExporterFn := func(_ TracingExporterOptions) (trace.Exporter, error) {
+		return nil, nil
+	}
+
+	_, err := GetTracingExporter("notexist", TracingExporterOptions{})
+	if !errdefs.IsNotFound(err) {
+		t.Fatalf("expected not found error, got: %v", err)
+	}
+
+	RegisterTracingExporter("mock", mockExporterFn)
+
+	if _, err := GetTracingExporter("mock", TracingExporterOptions{}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAvailableExporters(t *testing.T) {
+	defer delete(tracingExporters, "mock")
+
+	mockExporterFn := func(_ TracingExporterOptions) (trace.Exporter, error) {
+		return nil, nil
+	}
+	RegisterTracingExporter("mock", mockExporterFn)
+
+	for _, e := range AvailableTraceExporters() {
+		if e == "mock" {
+			return
+		}
+	}
+
+	t.Fatal("could not find mock exporter in list of registered exporters")
+}
