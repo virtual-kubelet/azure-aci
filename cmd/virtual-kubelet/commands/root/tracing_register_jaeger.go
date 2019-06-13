0a9f96e88607f683faffd762364835e8bcc78dcc
Brian Goff <cpuguy83@gmail.com>
2019-06-13 11:48:13 -0700

    Flip build tag for jaeger exporter
    No need to compile this in by default since it's for local debugging.

--- cmd/virtual-kubelet/commands/root/tracing_register_jaeger.go
+++ cmd/virtual-kubelet/commands/root/tracing_register_jaeger.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !no_jaeger_exporter
+// +build include_jaeger_exporter
 
 package root
 
