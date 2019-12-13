956b255f985208941f76bd582dae84f1bee12ba9
ibabou <ibabou@microsoft.com>
2019-12-13 14:15:30 -0800

    add constants

--- provider/podsTracker.go
+++ provider/podsTracker.go
@@ -12,6 +12,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+var (
+	podStatusReasonProviderFailed       = "ProviderFailed"
+	statusReasonNotFound                = "NotFound"
+	statusMessageNotFound               = "The pod may have been deleted from the provider"
+	containerExitCodeNotFound     int32 = -137
+)
+
 type podsTracker struct {
 	rm            *manager.ResourceManager
 	podFetcher    func(string, string) (*v1.Pod, error)
@@ -50,7 +57,7 @@ func (pt *podsTracker) updatePods(ctx context.Context) {
 	defer span.End()
 
 	k8sPods := pt.rm.GetPods()
-	// TODO check pods returned
+	log.G(ctx).Infof("%v pods returned", len(k8sPods))
 
 	for _, pod := range k8sPods {
 		if pt.shouldSkipPodStatusUpdate(pod) {
@@ -59,7 +66,7 @@ func (pt *podsTracker) updatePods(ctx context.Context) {
 
 		log.G(ctx).WithField("podName", pod.Name).Infof("processing pod updates...")
 
-		podStatus, err := pt.getPodStatusFromProvider(pod)
+		podStatus, _ := pt.getPodStatusFromProvider(pod)
 		// TODO: check failures
 
 		updatedPod := pod.DeepCopy()
@@ -71,7 +78,7 @@ func (pt *podsTracker) updatePods(ctx context.Context) {
 func (pt *podsTracker) shouldSkipPodStatusUpdate(pod *v1.Pod) bool {
 	return pod.Status.Phase == v1.PodSucceeded ||
 		pod.Status.Phase == v1.PodFailed ||
-		pod.Status.Reason == podStatusReasonProviderFailed // Pending because of failure
+		pod.Status.Reason == podStatusReasonProviderFailed // Pending phase because of failure
 }
 
 func (pt *podsTracker) getPodStatusFromProvider(podFromKubernetes *v1.Pod) (*v1.PodStatus, error) {
@@ -87,8 +94,8 @@ func (pt *podsTracker) getPodStatusFromProvider(podFromKubernetes *v1.Pod) (*v1.
 			// Set the pod to failed, this makes sure if the underlying container implementation is gone that a new pod will be created.
 			podStatus = podFromKubernetes.Status.DeepCopy()
 			podStatus.Phase = v1.PodFailed
-			podStatus.Reason = podStatusReasonNotFound
-			podStatus.Message = podStatusMessageNotFound
+			podStatus.Reason = statusReasonNotFound
+			podStatus.Message = statusMessageNotFound
 			now := metav1.NewTime(time.Now())
 			for i, c := range podStatus.ContainerStatuses {
 				if c.State.Running == nil {
@@ -96,9 +103,9 @@ func (pt *podsTracker) getPodStatusFromProvider(podFromKubernetes *v1.Pod) (*v1.
 				}
 
 				podStatus.ContainerStatuses[i].State.Terminated = &v1.ContainerStateTerminated{
-					ExitCode:    containerStatusExitCodeNotFound,
-					Reason:      containerStatusReasonNotFound,
-					Message:     containerStatusMessageNotFound,
+					ExitCode:    containerExitCodeNotFound,
+					Reason:      statusReasonNotFound,
+					Message:     statusMessageNotFound,
 					FinishedAt:  now,
 					StartedAt:   c.State.Running.StartedAt,
 					ContainerID: c.ContainerID,
