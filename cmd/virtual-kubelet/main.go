403733cc9ce7beaecaa5752213580d604f16db71
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2020-09-05 01:37:51 -0400

    update k8sVersion var to match k8s.io/kubernetes v1.18.4 import update from https://github.com/virtual-kubelet/azure-aci/pull/52

--- cmd/virtual-kubelet/main.go
+++ cmd/virtual-kubelet/main.go
@@ -34,7 +34,7 @@ import (
 var (
 	buildVersion    = "N/A"
 	buildTime       = "N/A"
-	k8sVersion      = "v1.14.3" // This should follow the version of k8s.io/kubernetes we are importing
+	k8sVersion      = "v1.18.4" // This should follow the version of k8s.io/kubernetes we are importing
 	numberOfWorkers = 50
 )
 
