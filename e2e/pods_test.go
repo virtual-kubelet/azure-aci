a161e72b1eae53a41e93a4ca5731ed0bca1c4336
t-ysalazar <t-ysalazar@microsoft.com>
2022-07-05 14:41:40 -0700

    e2e for init containers

--- e2e/pods_test.go
+++ e2e/pods_test.go
@@ -1,22 +1,12 @@
 package e2e
 
 import (
-	"bytes"
 	"testing"
 	"time"
 )
 
-func TestPodLifecycle(t *testing.T) {
-	// delete the pod first
-	kubectl("delete", "pod/vk-e2e-hpa")
-
-	spec, err := fixtures.ReadFile("fixtures/hpa.yml")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cmd := kubectl("apply", "-f", "-")
-	cmd.Stdin = bytes.NewReader(spec)
+func CreatePodFromKubectl(t *testing.T, podName string, podDir string) {
+	cmd := kubectl("apply", "-f", podDir)
 	if out, err := cmd.CombinedOutput(); err != nil {
 		t.Fatal(string(out))
 	}
@@ -26,20 +16,22 @@ func TestPodLifecycle(t *testing.T) {
 	if !ok {
 		timeout = 300 * time.Second
 	}
-	cmd = kubectl("wait", "--for=condition=ready", "--timeout="+timeout.String(), "pod/vk-e2e-hpa", "--namespace=vk-test")
+	cmd = kubectl("wait", "--for=condition=ready", "--timeout="+timeout.String(), "pod/"+podName, "--namespace=vk-test")
 	if out, err := cmd.CombinedOutput(); err != nil {
 		t.Fatal(string(out))
 	}
-	t.Log("success create pod")
 
-	// query metrics
-	deadline = time.Now().Add(5 * time.Minute)
+	t.Log("success create pod")
+}
+
+func QueryKubectlMetrics(t *testing.T, podName string) {
+	deadline := time.Now().Add(5 * time.Minute)
 	for {
 		t.Log("query metrics ....")
-		cmd = kubectl("get", "--raw", "/apis/metrics.k8s.io/v1beta1/namespaces/vk-test/pods/vk-e2e-hpa")
+		cmd := kubectl("get", "--raw", "/apis/metrics.k8s.io/v1beta1/namespaces/vk-test/pods/"+podName)
 		out, err := cmd.CombinedOutput()
 		if time.Now().After(deadline) {
-			t.Fatal("failed to query pod's stats from metris server API")
+			t.Fatal("failed to query pod's stats from metrics server API")
 		}
 		if err == nil {
 			t.Logf("success query metrics %s", string(out))
@@ -47,10 +39,28 @@ func TestPodLifecycle(t *testing.T) {
 		}
 		time.Sleep(10 * time.Second)
 	}
+}
 
+func CleanPodFromKubectl(t *testing.T, podName string) {
 	t.Log("clean up pod")
-	cmd = kubectl("delete", "pod/vk-e2e-hpa", "--namespace=vk-test")
+	cmd := kubectl("delete", "pod/"+podName, "--namespace=vk-test")
 	if out, err := cmd.CombinedOutput(); err != nil {
 		t.Fatal(string(out))
 	}
+}
+
+func TestPodLifecycle(t *testing.T) {
+	kubectl("delete", "pod/vk-e2e-hpa", "--namespace=vk-test")
+
+	CreatePodFromKubectl(t, "vk-e2e-hpa", "fixtures/hpa.yml")
+	//QueryKubectlMetrics(t, "vk-e2e-hpa")
+	CleanPodFromKubectl(t, "vk-e2e-hpa")
+}
+
+func TestInitContainerPod(t *testing.T) {
+	kubectl("delete", "pod/vk-e2e-initcontainers", "--namespace=vk-test")
+
+	CreatePodFromKubectl(t, "vk-e2e-initcontainers", "fixtures/initcontainers_pod.yml")
+	//QueryKubectlMetrics(t, "vk-e2e-initcontainers")
+	CleanPodFromKubectl(t, "vk-e2e-initcontainers")
 }
