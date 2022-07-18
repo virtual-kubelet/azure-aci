bfb5706e654392c83fe4c6082620f1472563d0bc
t-ysalazar <t-ysalazar@microsoft.com>
2022-07-18 13:17:17 -0700

    factorize TestImagePullUsingKubeletIdentity

--- e2e/deployments_test.go
+++ e2e/deployments_test.go
@@ -26,89 +26,21 @@ type KeyVault struct {
 	Value string `json:"value"`
 }
 
-func TestImagePullUsingKubeletIdentityAndSecrets(t *testing.T) {
-	cmd := kubectl("config", "current-context")
-	previousCluster, _ := cmd.CombinedOutput()
-
-	aksClusterName := "aksClusterE2E10"
-	managedIdentity := "e2eDeployTestMI10"
-
-	nodeName := "virtual-kubelet"
-	virtualNodeReleaseName := "virtual-kubelet-e2etest-aks"
-
-	vkRelease := "virtual-kubelet-latest"
-	chartURL := "https://github.com/virtual-kubelet/azure-aci/raw/master/charts/" + vkRelease + ".tgz"
-
-	managedIdentityURI := "/subscriptions/" + subscriptionID + "/resourcegroups/" + azureRG + "/providers/Microsoft.ManagedIdentity/userAssignedIdentities/" + managedIdentity
-
-	//create MI with role assignment
-	cmd = az("identity", "create", "--resource-group", azureRG, "--name", managedIdentity)
+func ConnectToAKSCluster(t *testing.T, clusterName string) {
+	cmd := az("aks", "get-credentials",
+		"--resource-group", azureRG,
+		"--name", clusterName,
+	)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatal(string(out))
 	}
+}
 
-	spID, _ := az("identity", "show", "--resource-group", azureRG, "--name", managedIdentity,
-		"--query", "principalId", "--output", "tsv").CombinedOutput()
+func GetCurrentClusterMasterURI(t *testing.T) string {
+	cmd := kubectl("cluster-info")
 
-	registryID, _ := az("acr", "show", "--resource-group", azureRG, "--name", containerRegistry, "--query",
-		"id", "--output", "tsv").CombinedOutput()
-
-	cmd = az("role", "assignment", "create", "--assignee-object-id", string(spID),
-		"--scope", string(registryID), "--role", "acrpull", "--assignee-principal-type", "ServicePrincipal")
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatal(string(out))
-	}
-
-	//get client secret
-	vaultName := "aci-virtual-node-test-kv"
-	secretName := "aci-virtualnode-sp-dev-credential"
-	cmd = az("keyvault", "secret", "show", "--name", secretName, "--vault-name", vaultName, "-o", "json")
-
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatal(string(out))
-	}
-
-	var keyvault KeyVault
-	json.Unmarshal(out, &keyvault)
-	azureClientSecret := keyvault.Value
-
-	//create cluster
-	cmd = az("aks", "create",
-		"--resource-group", azureRG,
-		"--name", aksClusterName,
-		"--node-count", "1",
-		"--network-plugin", "azure",
-		"--service-cidr", "10.0.0.0/16",
-		"--dns-service-ip", "10.0.0.10",
-		"--docker-bridge-address", "172.17.0.1/16",
-		"--enable-managed-identity",
-		"--assign-identity", managedIdentityURI,
-		"--assign-kubelet-identity", managedIdentityURI,
-	)
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatal(string(out))
-	}
-	t.Log("aks cluster created")
-
-	//connect cluster
-	cmd = az("aks", "get-credentials",
-		"--resource-group", azureRG,
-		"--name", aksClusterName,
-	)
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatal(string(out))
-	}
-	t.Log("connected to cluster")
-
-	//get master URI
-	cmd = kubectl("cluster-info")
-
-	out, err = cmd.CombinedOutput()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatal(string(out))
 	}
@@ -117,6 +49,30 @@ func TestImagePullUsingKubeletIdentityAndSecrets(t *testing.T) {
 	//delete invisible characters and save masterURI
 	re := regexp.MustCompile("\\x1B\\[[0-9;]*[a-zA-Z]")
 	masterURI := re.ReplaceAllString(clusterInfo, "")
+
+	return masterURI
+}
+
+func RunVirtualNodeLifeciyleUsingSecrets(t *testing.T, masterURI string) {
+	nodeName := "virtual-kubelet"
+	virtualNodeReleaseName := "virtual-kubelet-e2etest-aks"
+
+	vkRelease := "virtual-kubelet-latest"
+	chartURL := "https://github.com/virtual-kubelet/azure-aci/raw/master/charts/" + vkRelease + ".tgz"
+
+	//get client secret
+	vaultName := "aci-virtual-node-test-kv"
+	secretName := "aci-virtualnode-sp-dev-credential"
+	cmd := az("keyvault", "secret", "show", "--name", secretName, "--vault-name", vaultName, "-o", "json")
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(string(out))
+	}
+
+	var keyvault KeyVault
+	json.Unmarshal(out, &keyvault)
+	azureClientSecret := keyvault.Value
 
 	//create virtual node
 	cmd = helm("install", virtualNodeReleaseName, chartURL,
@@ -140,21 +96,71 @@ func TestImagePullUsingKubeletIdentityAndSecrets(t *testing.T) {
 	if err != nil {
 		t.Fatal(string(out))
 	}
-	t.Log(string(out))
 
 	//test pod lifecycle
 	CreatePodFromKubectl(t, "mi-pull-image", "fixtures/mi-pull-image.yaml")
 	DeletePodFromKubectl(t, "mi-pull-image")
 
-	t.Log("deleting")
-
-	kubectl("config", "use-context", string(previousCluster))
-
+	//delete virtual node
 	kubectl("delete", "deployments", "--all")
 	kubectl("delete", "pods", "--all")
 	kubectl("delete", "node", nodeName)
 	helm("uninstall", virtualNodeReleaseName)
+}
 
+func TestImagePullUsingKubeletIdentity(t *testing.T) {
+	cmd := kubectl("config", "current-context")
+	previousCluster, _ := cmd.CombinedOutput()
+
+	aksClusterName := "aksClusterE2E15"
+	managedIdentity := "e2eDeployTestMI15"
+
+	managedIdentityURI := "/subscriptions/" + subscriptionID + "/resourcegroups/" + azureRG + "/providers/Microsoft.ManagedIdentity/userAssignedIdentities/" + managedIdentity
+
+	//create MI with role assignment
+	cmd = az("identity", "create", "--resource-group", azureRG, "--name", managedIdentity)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(string(out))
+	}
+
+	spID, _ := az("identity", "show", "--resource-group", azureRG, "--name", managedIdentity,
+		"--query", "principalId", "--output", "tsv").CombinedOutput()
+
+	registryID, _ := az("acr", "show", "--resource-group", azureRG, "--name", containerRegistry, "--query",
+		"id", "--output", "tsv").CombinedOutput()
+
+	cmd = az("role", "assignment", "create", "--assignee-object-id", string(spID),
+		"--scope", string(registryID), "--role", "acrpull", "--assignee-principal-type", "ServicePrincipal")
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(string(out))
+	}
+
+	//create cluster
+	cmd = az("aks", "create",
+		"--resource-group", azureRG,
+		"--name", aksClusterName,
+		"--node-count", "1",
+		"--network-plugin", "azure",
+		"--service-cidr", "10.0.0.0/16",
+		"--dns-service-ip", "10.0.0.10",
+		"--docker-bridge-address", "172.17.0.1/16",
+		"--enable-managed-identity",
+		"--assign-identity", managedIdentityURI,
+		"--assign-kubelet-identity", managedIdentityURI,
+	)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(string(out))
+	}
+
+	ConnectToAKSCluster(t, aksClusterName)
+	masterURI := GetCurrentClusterMasterURI(t)
+
+	RunVirtualNodeLifeciyleUsingSecrets(t, masterURI)
+
+	kubectl("config", "use-context", string(previousCluster))
 	az("aks", "delete", "--name", aksClusterName, "--resource-group", azureRG, "--yes")
 	kubectl("config", "delete-context", aksClusterName)
 }
