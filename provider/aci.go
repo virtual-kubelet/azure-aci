b93be3dccf36504750877b3849f05fa36bdb816f
Arnav Arnav <fnuarnav@microsoft.com>
2022-08-05 16:30:45 -0400

    add RG and location to helm; add azure dns ip to aci request

--- provider/aci.go
+++ provider/aci.go
@@ -795,8 +795,10 @@ func (p *ACIProvider) getDNSConfig(pod *v1.Pod) *aci.DNSConfig {
 	nameServers := make([]string, 0)
 	searchDomains := []string{}
 
+	AzureDNSIP := "168.63.129.16"
 	if pod.Spec.DNSPolicy == v1.DNSClusterFirst || pod.Spec.DNSPolicy == v1.DNSClusterFirstWithHostNet {
 		nameServers = append(nameServers, p.kubeDNSIP)
+		nameServers = append(nameServers, AzureDNSIP)
 		searchDomains = p.generateSearchesForDNSClusterFirst(pod.Spec.DNSConfig, pod)
 	}
 
