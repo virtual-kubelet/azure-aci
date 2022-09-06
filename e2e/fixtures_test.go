a3ce27e30f414fbce8b5c50b7285075eca20d1cc
Arnav Arnav <fnuarnav@microsoft.com>
2022-09-06 19:41:26 -0400

    removed unused code

--- e2e/fixtures_test.go
+++ e2e/fixtures_test.go
@@ -6,29 +6,9 @@ import (
 	"regexp"
 )
 
-//delete invisible characters
-func cleanString(toClean string) string {
-	re := regexp.MustCompile(`\x1B\[[0-9;]*[a-zA-Z]`)
-	return re.ReplaceAllString(toClean, "")
-}
-
 //execute kubectl command in terminal
 func kubectl(args ...string) *exec.Cmd {
 	cmd := exec.Command("kubectl", args...)
-	cmd.Env = os.Environ()
-	return cmd
-}
-
-//execute helm command in terminal
-func helm(args ...string) *exec.Cmd {
-	cmd := exec.Command("helm", args...)
-	cmd.Env = os.Environ()
-	return cmd
-}
-
-//execute az command in terminal
-func az(args ...string) *exec.Cmd {
-	cmd := exec.Command("az", args...)
 	cmd.Env = os.Environ()
 	return cmd
 }
