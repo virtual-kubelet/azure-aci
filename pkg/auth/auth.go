aa870495316984f0214be907493c10b5a72b4a9e
Heba Elayoty <hebaelayoty@gmail.com>
2022-09-26 12:11:10 -0700

    Migrate to Azure SDK for auth

---
+++ pkg/auth/auth.go
@@ -0,0 +1,264 @@
+package auth
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"unicode/utf16"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
+	"github.com/dimchansky/utfbom"
+	"github.com/virtual-kubelet/virtual-kubelet/log"
+)
+
+type CloudEnvironmentName string
+
+var (
+	AzureGermany = cloud.Configuration{
+		ActiveDirectoryAuthorityHost: "https://login.microsoftonline.de/", Services: map[cloud.ServiceName]cloud.ServiceConfiguration{},
+	}
+)
+
+const (
+	AzurePublicCloud       CloudEnvironmentName = "AzurePublicCloud"
+	AzureUSGovernmentCloud CloudEnvironmentName = "AzureUSGovernment"
+	AzureGermanyCloud      CloudEnvironmentName = "AzureGermany"
+	AzureChinaCloud        CloudEnvironmentName = "AzureChina"
+)
+
+type Config struct {
+	AcsCredential *acsCredential
+	AuthConfig    *Authentication
+	Cloud         cloud.Configuration
+	Authorizer    autorest.Authorizer
+}
+
+// getAuthorizer return autorest authorizer
+func (c *Config) getAuthorizer(resource string) (autorest.Authorizer, error) {
+	var auth autorest.Authorizer
+	var err error
+
+	var token *adal.ServicePrincipalToken
+	if c.AuthConfig.UseUserIdentity {
+		token, err = adal.NewServicePrincipalTokenFromManagedIdentity(
+			resource, &adal.ManagedIdentityOptions{ClientID: c.AuthConfig.UserIdentityClientId})
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		oauthConfig, err := adal.NewOAuthConfig(
+			c.Cloud.ActiveDirectoryAuthorityHost, c.AuthConfig.TenantID)
+		if err != nil {
+			return nil, err
+		}
+		token, err = adal.NewServicePrincipalToken(
+			*oauthConfig, c.AuthConfig.ClientID, c.AuthConfig.ClientSecret, resource)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	auth = autorest.NewBearerAuthorizer(token)
+	return auth, err
+}
+
+// SetAuthConfig sets the configuration needed for Authentication
+func (c *Config) SetAuthConfig() error {
+	var err error
+	c.Cloud = cloud.AzurePublic
+
+	if authFilepath := os.Getenv("AZURE_AUTH_LOCATION"); authFilepath != "" {
+		auth := &Authentication{}
+		err = auth.NewAuthenticationFromFile(authFilepath)
+		if err != nil {
+			return err
+		}
+		c.AuthConfig = auth
+	}
+
+	if acsFilepath := os.Getenv("ACS_CREDENTIAL_LOCATION"); acsFilepath != "" {
+		c.AcsCredential, err = NewAcsCredential(acsFilepath)
+		if err != nil {
+			return err
+		}
+
+		if c.AcsCredential != nil {
+			var clientId string
+			if !strings.EqualFold(c.AcsCredential.ClientID, "msi") {
+				clientId = c.AcsCredential.ClientID
+			}
+
+			//Set Azure cloud environment
+			c.Cloud = getCloudConfiguration(c.AcsCredential.Cloud)
+			c.AuthConfig = NewAuthentication(
+				clientId,
+				c.AcsCredential.ClientSecret,
+				c.AcsCredential.SubscriptionID,
+				c.AcsCredential.TenantID,
+				c.AcsCredential.UserAssignedIdentityID)
+		}
+
+		if clientID := os.Getenv("AZURE_CLIENT_ID"); clientID != "" {
+			c.AuthConfig.ClientID = clientID
+		}
+
+		if clientSecret := os.Getenv("AZURE_CLIENT_SECRET"); clientSecret != "" {
+			c.AuthConfig.ClientSecret = clientSecret
+		}
+
+		if userIdentityClientId := os.Getenv("VIRTUALNODE_USER_IDENTITY_CLIENTID"); userIdentityClientId != "" {
+			c.AuthConfig.UserIdentityClientId = userIdentityClientId
+		}
+		c.AuthConfig.UseUserIdentity = (len(c.AuthConfig.ClientID) == 0)
+
+		if c.AuthConfig.UseUserIdentity {
+			if len(c.AuthConfig.UserIdentityClientId) == 0 {
+				return fmt.Errorf("neither AZURE_CLIENT_ID or VIRTUALNODE_USER_IDENTITY_CLIENTID is being set")
+			}
+
+			log.G(context.TODO()).Info("Using user identity for Authentication")
+		}
+
+		if tenantID := os.Getenv("AZURE_TENANT_ID"); tenantID != "" {
+			c.AuthConfig.TenantID = tenantID
+		}
+
+		if subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID"); subscriptionID != "" {
+			c.AuthConfig.SubscriptionID = subscriptionID
+		}
+	}
+
+	resource := c.Cloud.Services[cloud.ResourceManager].Endpoint
+	options := &arm.ClientOptions{}
+
+	if cs, ok := options.Cloud.Services[cloud.ResourceManager]; ok {
+		resource = cs.Endpoint
+	}
+
+	c.Authorizer, err = c.getAuthorizer(resource)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Authentication represents the Authentication file for Azure.
+type Authentication struct {
+	ClientID             string `json:"clientId,omitempty"`
+	ClientSecret         string `json:"clientSecret,omitempty"`
+	SubscriptionID       string `json:"subscriptionId,omitempty"`
+	TenantID             string `json:"tenantId,omitempty"`
+	UseUserIdentity      bool   `json:"useUserIdentity,omitempty"`
+	UserIdentityClientId string `json:"userIdentityClientId,omitempty"`
+}
+
+// NewAuthenticationFromFile returns an Authentication struct from file path
+func (a *Authentication) NewAuthenticationFromFile(filepath string) error {
+	b, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return fmt.Errorf("reading Authentication file %q failed: %v", filepath, err)
+	}
+
+	// Authentication file might be encoded.
+	decoded, err := a.decode(b)
+	if err != nil {
+		return fmt.Errorf("decoding Authentication file %q failed: %v", filepath, err)
+	}
+
+	// Unmarshal the Authentication file.
+	if err := json.Unmarshal(decoded, &a); err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewAuthentication returns an Authentication struct from user provided credentials.
+func NewAuthentication(clientID, clientSecret, subscriptionID, tenantID, userAssignedIdentityID string) *Authentication {
+
+	return &Authentication{
+		ClientID:             clientID,
+		ClientSecret:         clientSecret,
+		SubscriptionID:       subscriptionID,
+		TenantID:             tenantID,
+		UserIdentityClientId: userAssignedIdentityID,
+	}
+}
+
+// acsCredential represents the credential file for ACS
+type acsCredential struct {
+	Cloud                  string `json:"cloud"`
+	TenantID               string `json:"tenantId"`
+	SubscriptionID         string `json:"subscriptionId"`
+	ClientID               string `json:"aadClientId"`
+	ClientSecret           string `json:"aadClientSecret"`
+	ResourceGroup          string `json:"resourceGroup"`
+	Region                 string `json:"location"`
+	VNetName               string `json:"vnetName"`
+	VNetResourceGroup      string `json:"vnetResourceGroup"`
+	UserAssignedIdentityID string `json:"userAssignedIdentityID"`
+}
+
+// NewAcsCredential returns an acsCredential struct from file path
+func NewAcsCredential(p string) (*acsCredential, error) {
+	logger := log.G(context.TODO()).WithField("method", "NewAcsCredential").WithField("file", p)
+	logger.Debug("Reading ACS credential file")
+
+	b, err := ioutil.ReadFile(p)
+	if err != nil {
+		return nil, fmt.Errorf("reading ACS credential file %q failed: %v", p, err)
+	}
+
+	// Unmarshal the Authentication file.
+	var cred acsCredential
+	if err := json.Unmarshal(b, &cred); err != nil {
+		return nil, err
+	}
+
+	logger.Debug("Load ACS credential file successfully")
+	return &cred, nil
+}
+
+func (a *Authentication) decode(b []byte) ([]byte, error) {
+	reader, enc := utfbom.Skip(bytes.NewReader(b))
+
+	switch enc {
+	case utfbom.UTF16LittleEndian:
+		u16 := make([]uint16, (len(b)/2)-1)
+		err := binary.Read(reader, binary.LittleEndian, &u16)
+		if err != nil {
+			return nil, err
+		}
+		return []byte(string(utf16.Decode(u16))), nil
+	case utfbom.UTF16BigEndian:
+		u16 := make([]uint16, (len(b)/2)-1)
+		err := binary.Read(reader, binary.BigEndian, &u16)
+		if err != nil {
+			return nil, err
+		}
+		return []byte(string(utf16.Decode(u16))), nil
+	}
+	return ioutil.ReadAll(reader)
+}
+
+func getCloudConfiguration(cloudName string) cloud.Configuration {
+	switch cloudName {
+	case string(AzurePublicCloud):
+		return cloud.AzurePublic
+	case string(AzureUSGovernmentCloud):
+		return cloud.AzureGovernment
+	case string(AzureChinaCloud):
+		return cloud.AzureChina
+	case string(AzureGermanyCloud):
+		return AzureGermany
+	}
+	panic("cloud config does not exist")
+}
