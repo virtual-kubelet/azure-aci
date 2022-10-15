00fa64f68207e8146df5136718bc08d318924017
Heba Elayoty <hebaelayoty@gmail.com>
2022-10-14 18:10:10 -0700

    Add more UT for logAnalytics

--- pkg/analytics/analytics.go
+++ pkg/analytics/analytics.go
@@ -4,14 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"os"
 
 	azaci "github.com/Azure/azure-sdk-for-go/services/containerinstance/mgmt/2021-10-01/containerinstance"
 )
 
 // NewContainerGroupDiagnostics creates a container group diagnostics object
 func NewContainerGroupDiagnostics(logAnalyticsID, logAnalyticsKey string) (*azaci.ContainerGroupDiagnostics, error) {
-
 	if logAnalyticsID == "" || logAnalyticsKey == "" {
 		return nil, errors.New("log Analytics configuration requires both the workspace ID and Key")
 	}
@@ -26,18 +25,29 @@ func NewContainerGroupDiagnostics(logAnalyticsID, logAnalyticsKey string) (*azac
 
 // NewContainerGroupDiagnosticsFromFile creates a container group diagnostics object from the specified file
 func NewContainerGroupDiagnosticsFromFile(filepath string) (*azaci.ContainerGroupDiagnostics, error) {
-
-	analyticsdata, err := ioutil.ReadFile(filepath)
+	analyticsDataFile, err := os.Open(filepath)
+	defer analyticsDataFile.Close()
 	if err != nil {
-		return nil, fmt.Errorf("Reading Log Analytics Auth file %q failed: %v", filepath, err)
+		return nil, errors.New(fmt.Sprintf("reading Log Analytics Auth file %q failed: %v", filepath, err))
 	}
+
+	fileStat, err := analyticsDataFile.Stat()
+	if fileStat.Size() == 0 {
+		return nil, fmt.Errorf("log analytics auth file %q cannot be empty", filepath)
+	}
+
+	analyticsData, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("reading Log Analytics Auth file %q failed: %v", filepath, err)
+	}
+
 	// Unmarshal the log analytics file.
-	var law azaci.LogAnalytics
-	if err := json.Unmarshal(analyticsdata, &law); err != nil {
+	var logAnalytics azaci.LogAnalytics
+	if err := json.Unmarshal(analyticsData, &logAnalytics); err != nil {
 		return nil, err
 	}
 
 	return &azaci.ContainerGroupDiagnostics{
-		LogAnalytics: &law,
-	}, nil
+		LogAnalytics: &logAnalytics,
+	}, err
 }
