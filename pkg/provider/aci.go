d1281fd1e980905eb5c67f87e30184bc6c1393fa
Heba Elayoty <hebaelayoty@gmail.com>
2022-11-14 12:36:02 -0800

    move back to non async

--- pkg/provider/aci.go
+++ pkg/provider/aci.go
@@ -381,6 +381,7 @@ func (p *ACIProvider) DeletePod(ctx context.Context, pod *v1.Pod) error {
 	ctx = addAzureAttributes(ctx, span, p)
 
 	log.G(ctx).Infof("start deleting pod %v", pod.Name)
+	// TODO: Run in a go routine to not block workers.
 	return p.deleteContainerGroup(ctx, pod.Namespace, pod.Name)
 }
 
