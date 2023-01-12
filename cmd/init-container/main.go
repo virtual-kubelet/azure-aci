cbb2e27ce6268ef006e721f7ce8eaee42c0f1927
Heba Elayoty <hebaelayoty@gmail.com>
2023-01-11 16:13:57 -0800

    Remove provider obj and use wait.Until
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>

--- cmd/init-container/main.go
+++ cmd/init-container/main.go
@@ -9,12 +9,10 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/virtual-kubelet/azure-aci/pkg/auth"
 	"github.com/virtual-kubelet/azure-aci/pkg/network"
-	"github.com/virtual-kubelet/azure-aci/pkg/provider"
 	cli "github.com/virtual-kubelet/node-cli"
 	"github.com/virtual-kubelet/virtual-kubelet/log"
 	logruslogger "github.com/virtual-kubelet/virtual-kubelet/log/logrus"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/util/retry"
 )
 
 func main() {
@@ -29,37 +27,26 @@ func main() {
 		return
 	}
 
-	setupBackoff := wait.Backoff{
-		Steps:    10,
-		Duration: 10 * time.Millisecond,
-		Factor:   0,
-		Jitter:   0.1,
-	}
+	wait.Until(func() {
+		azConfig := auth.Config{}
 
-	retry.OnError(setupBackoff,
-		func(err error) bool { return true },
-		func() error {
-			azConfig := auth.Config{}
-
-			if vkVersion {
-				//Setup config
-				err = azConfig.SetAuthConfig(ctx)
-				if err != nil {
-					log.G(ctx).Fatalf("init container: cannot setup the auth configuration ", err)
-				}
-			}
-			p := provider.ACIProvider{
-				ProviderNetwork: network.ProviderNetwork{},
-			}
-
-			// Check or set up a network for VK
-			log.G(ctx).Info("init container: setting up the network configuration")
-			err = p.ProviderNetwork.SetVNETConfig(ctx, &azConfig)
+		if vkVersion {
+			//Setup config
+			err = azConfig.SetAuthConfig(ctx)
 			if err != nil {
-				log.G(ctx).Fatalf("init container: cannot setup the VNet configuration ", err)
+				log.G(ctx).Fatalf("init container: cannot setup the auth configuration ", err)
 			}
+		}
 
-			return nil
-		})
+		var providerNetwork network.ProviderNetwork
+		// Check or set up a network for VK
+		log.G(ctx).Info("init container: setting up the network configuration")
+		err = providerNetwork.SetVNETConfig(ctx, &azConfig)
+		if err != nil {
+			log.G(ctx).Fatalf("init container: cannot setup the VNet configuration ", err)
+		}
+	},
+		1*time.Minute, ctx.Done())
+
 	log.G(ctx).Info("initial setup for virtual kubelet Azure ACI is successful")
 }
