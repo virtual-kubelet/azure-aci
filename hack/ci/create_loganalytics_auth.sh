a51c2eed6b213f539ced46cf49f765eb144f7bdf
Heba Elayoty <hebaelayoty@gmail.com>
2023-03-22 12:04:28 -0700

    Update unit test target for local running
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>

--- hack/ci/create_loganalytics_auth.sh
+++ hack/ci/create_loganalytics_auth.sh
@@ -7,7 +7,7 @@ mkdir -p $(dirname "${TEST_LOGANALYTICS_JSON}")
 # This will build the log analytics credentials during CI
 cat <<EOF > "${TEST_LOGANALYTICS_JSON}"
 {
-    "workspaceID": "$omsworkspaceID",
-    "workspaceKey": "$omsworkspaceKey"
+  "workspaceId": "######-###-####-####-######",
+  "workspaceKey": "######-###-####-####-######"
 }
 EOF
