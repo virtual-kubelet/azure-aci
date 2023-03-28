3752b643237148493dc1a14a018206d1ccaa3cc8
Smriti Dahal <smritidahal@microsoft.com>
2023-03-28 16:16:45 -0700

    refactored CreatePodsList to utils.go

--- pkg/provider/podsTracker_test.go
+++ pkg/provider/podsTracker_test.go
@@ -4,18 +4,17 @@ import (
 	"context"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
+	testsutil "github.com/virtual-kubelet/azure-aci/pkg/tests"
 	"gotest.tools/assert"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestUpdatePodStatus(t *testing.T) {
 	podNames:= []string{"p1", "p2"}
+	podNamespace := "ns-" + uuid.New().String()
 
 	cases := []struct {
 		description		string
@@ -43,14 +42,14 @@ func TestUpdatePodStatus(t *testing.T) {
 			defer mockCtrl.Finish()
 
 			podLister := NewMockPodLister(mockCtrl)
-			podLister.EXPECT().List(gomock.Any()).Return(fakePods(podNames), nil)
+			podLister.EXPECT().List(gomock.Any()).Return(testsutil.CreatePodsList(podNames, podNamespace), nil)
 
 			podsTracker := &PodsTracker {
 				pods: podLister,
 				updateCb: func(p *v1.Pod) {},
 			}
 
-			err := podsTracker.UpdatePodStatus(context.Background(), "ns", tc.podName, func(podStatus *v1.PodStatus){}, true)
+			err := podsTracker.UpdatePodStatus(context.Background(), podNamespace, tc.podName, func(podStatus *v1.PodStatus){}, true)
 			if !tc.shouldFail {
 				assert.Check(t, err == nil, "Not expected to return error")
 			} else {
@@ -59,23 +58,4 @@ func TestUpdatePodStatus(t *testing.T) {
 			}
 		})
 	}
-}
-
-func fakePods(podNames []string) []*v1.Pod {
-	result := make([]*v1.Pod, 0, len(podNames))
-	for _, podName := range podNames {
-		pod := &v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:              podName,
-				Namespace:         "ns",
-				CreationTimestamp: metav1.NewTime(time.Now()),
-				UID:               types.UID(uuid.New().String()),
-			},
-			Status: v1.PodStatus{
-				Phase: v1.PodRunning,
-			},
-		}
-		result = append(result, pod)
-	}
-	return result
 }
