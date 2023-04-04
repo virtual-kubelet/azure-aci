b35dabe0de5fd4fe69acde401a0dec4e3bddf237
Nishant Dhungel <nidhunge@microsoft.com>
2023-04-04 12:42:26 -0700

    Added UT for real_time.go for CalculateUsageNanoCores

---
+++ pkg/metrics/real_time_test.go
@@ -0,0 +1,103 @@
+package metrics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculateUsageNanoCores(t *testing.T) {
+
+	fake_container1 := "fake-container-name1"
+	fake_container2 := "fake-container-name2"
+
+	newPodStatus_containers := []containerStats{
+		{
+			Name: fake_container1,
+			CPU: cpuStats{
+				UsageCoreNanoSeconds: 2345678900,
+			},
+		},
+		{
+			Name: fake_container2,
+			CPU: cpuStats{
+				UsageCoreNanoSeconds: 2345678900,
+			},
+		},
+	}
+
+	lastPodStatus_containers := []containerStats{
+		{
+			Name: fake_container1,
+			CPU: cpuStats{
+				UsageCoreNanoSeconds: 1234567800,
+			},
+		},
+		{
+			Name: fake_container2,
+			CPU: cpuStats{
+				UsageCoreNanoSeconds: 1234567800,
+			},
+		},
+	}
+
+	testCases := []struct {
+		desc          string
+		containerName *string
+		lastPodStatus *realtimeMetricsExtensionPodStats
+		newPodStatus  *realtimeMetricsExtensionPodStats
+		expectedUsage *uint64
+	}{
+		{
+			desc:          "NewPodStatus timestamp is earlier than LastPodStatus",
+			containerName: nil,
+			lastPodStatus: &realtimeMetricsExtensionPodStats{
+				Timestamp: 2345000000,
+			},
+			newPodStatus: &realtimeMetricsExtensionPodStats{
+				Timestamp: 1234000000,
+			},
+			expectedUsage: newUInt64Pointer(0),
+		},
+		{
+			desc:          "Container Name is nil",
+			containerName: nil,
+			lastPodStatus: &realtimeMetricsExtensionPodStats{
+				Timestamp: 1234500000,
+				CPU: cpuStats{
+					UsageCoreNanoSeconds: 1234567000,
+				},
+			},
+			newPodStatus: &realtimeMetricsExtensionPodStats{
+				Timestamp: 2345600000,
+				CPU: cpuStats{
+					UsageCoreNanoSeconds: 2345678000,
+				},
+			},
+			//tc.newPodStatus.CPU.UsageCoreNanoSeconds-tc.lastPodStatus.CPU.UsageCoreNanoSeconds/((tc.newPodStatus.TimeStamp-tc.lastPodStatus.TimeStamp)/1000000000)
+			expectedUsage: newUInt64Pointer(1111111000 / (1111100000 / 1000000000)),
+		},
+		{
+			desc:          "Container Name is not nil",
+			containerName: &fake_container1,
+			lastPodStatus: &realtimeMetricsExtensionPodStats{
+				Timestamp:  1234560000,
+				Containers: lastPodStatus_containers,
+			},
+			newPodStatus: &realtimeMetricsExtensionPodStats{
+				Timestamp:  2345670000,
+				Containers: newPodStatus_containers,
+			},
+			//tc.newPodStatus.containers[i=0].CPU.UsageCoreNanoSeconds-tc.lastPodStatus.containers[i=0].CPU.UsageCoreNanoSeconds/((tc.newPodStatus.TimeStamp-tc.lastPodStatus.TimeStamp)/1000000000)
+			expectedUsage: newUInt64Pointer(1111111100 / (1111110000 / 1000000000)),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			nanoCoreUsage := calculateUsageNanoCores(tc.containerName, tc.lastPodStatus, tc.newPodStatus)
+			assert.EqualValues(t, tc.expectedUsage, nanoCoreUsage, tc.desc)
+		})
+	}
+
+}
