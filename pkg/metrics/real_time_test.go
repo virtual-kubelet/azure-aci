975d6464a9fe33afd5203823656754624c1c8707
Nishant Dhungel <nidhunge@microsoft.com>
2023-04-10 16:20:39 -0700

    Added UT for FilterOutContainersNotInPod in real_time.go

--- pkg/metrics/real_time_test.go
+++ pkg/metrics/real_time_test.go
@@ -3,7 +3,10 @@ package metrics
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestCalculateUsageNanoCores(t *testing.T) {
@@ -146,5 +149,67 @@ func TestCalculateUsageNanoCores(t *testing.T) {
 			assert.EqualValues(t, tc.expectedUsage, nanoCoreUsage, tc.desc)
 		})
 	}
+
+}
+
+func TestFilterOutContainerNotInPod(t *testing.T) {
+
+	fake_container1 := "fake-container-name1"
+	fake_container2 := "fake-container-name2"
+	fake_container3 := "fake-infra-sidecar-container"
+	fake_container4 := "fake-container-name4"
+	podName := "pod-" + uuid.New().String()
+	podNamespace := "ns-" + uuid.New().String()
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: podNamespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name: fake_container1,
+				},
+				{
+					Name: fake_container2,
+				},
+				{
+					Name: fake_container4,
+				},
+			},
+		},
+	}
+
+	podStats := &realtimeMetricsExtensionPodStats{
+		Timestamp: 1234560000,
+		Containers: []containerStats{
+			{
+				Name: fake_container1,
+				CPU: cpuStats{
+					UsageCoreNanoSeconds: 2345678900,
+				},
+			},
+			{
+				Name: fake_container2,
+				CPU: cpuStats{
+					UsageCoreNanoSeconds: 1234567800,
+				},
+			},
+			{
+				Name: fake_container3,
+				CPU: cpuStats{
+					UsageCoreNanoSeconds: 9876543210,
+				},
+			},
+		},
+	}
+
+	testDescription := "Sucessfully filters out containerStats for containers not in pod.spec.containers"
+	t.Run(testDescription, func(t *testing.T) {
+		filterOutContainerNotInPod(podStats, pod)
+		assert.EqualValues(t, len(podStats.Containers), 2)
+		assert.EqualValues(t, podStats.Containers[0].Name, fake_container1)
+		assert.EqualValues(t, podStats.Containers[1].Name, fake_container2)
+	})
 
 }
